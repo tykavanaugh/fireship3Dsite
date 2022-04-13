91befd040ca6cb1164f017687cbb09732e790db8
tykavanaugh <tykavanaugh@gmail.com>
2022-04-13 09:15:04 -0400

    not working

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 import './style.css'
+import * as THREE from 'three'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
+const renderer = new THREE.WebGL1Renderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize( window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene,camera);
+
+const geometry = new THREE.TorusGeometry(10,3,16,100)
+const material = new THREE.MeshBasicMaterial({color:0xFF6347,wireframe:true});
+const torus = new THREE.Mesh(geometry,material);
+
+scene.add(torus);
+renderer.render(scene,camera);
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  renderer.render(scene,camera);
+}
+
+animate()
